876edd60d0749e9504c9764dd0e7f031fe250617
Sebastian Kurfuerst <sebastian@typo3.org>
2011-12-31 17:34:08 +0100

    improvements

--- flow3.plugin.zsh
+++ flow3.plugin.zsh
@@ -1,5 +1,10 @@
 _flow3() {
-  if [ -f flow3 ]; then
+  if _flow3_is_inside_base_distribution; then
+
+    local startDirectory=`pwd`
+    while [ ! -f flow3 ]; do
+      cd ..
+    done
     if (( $CURRENT > 2 )); then
       CURRENT=$CURRENT-1
       local cmd=${words[2]}
@@ -9,6 +14,7 @@ _flow3() {
     else
       _flow3_main_commands
     fi
+    cd $startDirectory
   fi
 }
 
@@ -31,7 +37,67 @@ _flow3_subcommand() {
   compadd -x "`cat Data/Temporary/Development/.flow3-autocompletion-command-$cmd`"
 }
 
+_flow3_is_inside_base_distribution() {
+  local startDirectory=`pwd`
+  while [[ ! -f flow3 ]]; do
 
+    if [[ `pwd` == "/" ]]; then
+      cd $startDirectory
+      return 1
+    fi
+    cd ..
+  done
+  cd $startDirectory
+  return 0
+}
 
+# FLOW3 command which can be executed inside sub directories of
+# the FLOW3 distribution
+flow3() {
+  if _flow3_is_inside_base_distribution; then
+  else
+    echo "ERROR: FLOW3 not found inside a parent of current directory"
+    return 1
+  fi
+
+  local startDirectory=`pwd`
+  while [ ! -f flow3 ]; do
+    cd ..
+  done
+  ./flow3 $@
+  cd $startDirectory
+}
+
+f3unittest() {
+  if _flow3_is_inside_base_distribution; then
+  else
+    echo "ERROR: FLOW3 not found inside a parent of current directory"
+    return 1
+  fi
+
+  local startDirectory=`pwd`
+  while [ ! -f flow3 ]; do
+    cd ..
+  done
+  local flow3BaseDir=`pwd`
+  cd $startDirectory
+  phpunit -c $flow3BaseDir/Build/Common/PhpUnit/UnitTests.xml --colors $@
+}
+
+f3functionaltest() {
+  if _flow3_is_inside_base_distribution; then
+  else
+    echo "ERROR: FLOW3 not found inside a parent of current directory"
+    return 1
+  fi
+
+  local startDirectory=`pwd`
+  while [ ! -f flow3 ]; do
+    cd ..
+  done
+  local flow3BaseDir=`pwd`
+  cd $startDirectory
+  phpunit -c $flow3BaseDir/Build/Common/PhpUnit/FunctionalTests.xml --colors $@}
+}
 
 compdef _flow3 flow3
